967a7c644d6d370cbea5e1d0f0091032615605eb
yuorei <108039575+yuorei@users.noreply.github.com>
2023-08-29 10:59:00 +0900

    エンドポイントをgraphqlに変更

--- server.go
+++ server.go
@@ -7,8 +7,8 @@ import (
 
 	"github.com/99designs/gqlgen/graphql/handler"
 	"github.com/99designs/gqlgen/graphql/playground"
-	"github.com/yuorei/hackathon/graph"
 	"github.com/yuorei/hackathon/graph/generated"
+	"github.com/yuorei/hackathon/graph/resolver"
 )
 
 const defaultPort = "8080"
@@ -19,10 +19,10 @@ func main() {
 		port = defaultPort
 	}
 
-	srv := handler.NewDefaultServer(generated.NewExecutableSchema(generated.Config{Resolvers: &graph.Resolver{}}))
+	srv := handler.NewDefaultServer(generated.NewExecutableSchema(generated.Config{Resolvers: &resolver.Resolver{}}))
 
-	http.Handle("/", playground.Handler("GraphQL playground", "/query"))
-	http.Handle("/query", srv)
+	http.Handle("/", playground.Handler("GraphQL playground", "/graphql"))
+	http.Handle("/graphql", srv)
 
 	log.Printf("connect to http://localhost:%s/ for GraphQL playground", port)
 	log.Fatal(http.ListenAndServe(":"+port, nil))
