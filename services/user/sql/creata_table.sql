7c842f24bd1b767cbea36f4532a47252aa5bdbcf
yuorei <108039575+yuorei@users.noreply.github.com>
2023-09-10 19:32:18 +0900

    emailをuniqueに変更

--- services/user/sql/creata_table.sql
+++ services/user/sql/creata_table.sql
@@ -1,7 +1,7 @@
 CREATE TABLE USERS (
     id INT NOT NULL AUTO_INCREMENT,
     name VARCHAR(100) NOT NULL,
-    email VARCHAR(100) NOT NULL,
+    email VARCHAR(100) NOT NULL UNIQUE,
     hashed_password VARCHAR(100) NOT NULL,
     gender ENUM('MAN','WOMAN','OTHER'),
     affiliation ENUM('STUDENT','OTHER'),
