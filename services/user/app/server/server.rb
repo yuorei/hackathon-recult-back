8a91cdabfb77ec3782922a154a0da7c51b93cd68
yuorei <108039575+yuorei@users.noreply.github.com>
2023-09-10 09:44:25 +0900

    サーバーがスタートしたことを示すようにした

--- services/user/app/server/server.rb
+++ services/user/app/server/server.rb
@@ -5,6 +5,7 @@ def main
     server = GRPC::RpcServer.new
     server.add_http2_port('0.0.0.0:50051', :this_port_is_insecure)
     server.handle(UserServiceImpl.new)
+    puts "Server running"
     server.run_till_terminated
 end
 
